9d13dc02a6f0f127139f222ddc4de11c62a9a89c
twlightloki <twlightloki@gmail.com>
2023-03-03 01:41:35 +0800

    fix mount_device

--- scripts/env_configure/mount_device.sh
+++ scripts/env_configure/mount_device.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 mount_source=$(lsblk -l -o NAME | grep -e sda1 -e sdb1 -e sdc1 | head -n 1)
-if [ $(expr length "${mount_source}") == 0  ]; then
-    echo "没有检测到硬盘挂载" 
-    exit 1
-else
-    sudo umount /dev/${mount_source}
+device_root=$(lsblk -l -o NAME,MOUNTPOINT | grep -e sda1 -e sdb1 -e sdc1 | head -n 1| rev | cut -d " " -f1 | rev)
+if [[ ${device_root} != "/media/nvidia/record" ]]; then
+    if [ $(expr length "${mount_source}") == 0  ]; then
+        echo "没有检测到硬盘挂载" 
+        exit 1
+    else
+        sudo umount /dev/${mount_source}
+    fi
+    echo "挂载硬盘 ${mount_source} to /media/nvidia/record/"
+    sudo mount /dev/${mount_source} /media/nvidia/record
 fi
-echo "挂载硬盘 ${mount_source} to /media/nvidia/record/"
-sudo mount /dev/${mount_source} /media/nvidia/record
-export DEVICE_ROOT=/media/nvidia/record/
+export DEVICE_ROOT=/media/nvidia/record
